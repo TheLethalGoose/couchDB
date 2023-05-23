22893caebb26822a86e9c163e9d704cde33ae872
Fabian Teufel <fabianteufel@me.com>
2023-05-23 11:49:40 +0200

    Working couchInitializer

--- src/main/java/de/fh/dortmund/Main.java
+++ src/main/java/de/fh/dortmund/Main.java
@@ -1,31 +1,11 @@
 package de.fh.dortmund;
 
-import com.google.gson.Gson;
 import de.fh.dortmund.couchDB.couchInitializer;
-import de.fh.dortmund.models.User;
-import de.fh.dortmund.service.POST;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.io.IOException;
-import java.net.URI;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        URI uri = URI.create("http://admin:admin@localhost:5984/");
+    public static void main(String[] args) {
 
-        couchInitializer.init(uri, "stackoverflow", true);
-
-        User user = new User("test@test", "test", "test", 0);
-
-        POST post = new POST("stackoverflow");
-        System.out.println(post.POST(user));
-
+        couchInitializer.init("localhost", 5984, "stackoverflow", "admin", "admin");
 
     }
 }
--- src/main/java/de/fh/dortmund/couchDB/couchInitializer.java
+++ src/main/java/de/fh/dortmund/couchDB/couchInitializer.java
@@ -1,38 +1,74 @@
 package de.fh.dortmund.couchDB;
 
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 
 public class couchInitializer {
 
-    public static void init(URI uri, String databaseName, boolean flushData) {
+    public static void init(String host, int port, String databaseName, String user, String password) {
 
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPut request = new HttpPut("http://localhost:5984/" + databaseName);
+        // Configuration of the target host
+        HttpHost target = new HttpHost(host, port, "http");
+
+        // On default couchDB only allows admin users to create databases
+
+        // Creating the AuthCache and Basic authentication
+        AuthCache authCache = new BasicAuthCache();
+        BasicScheme basicAuth = new BasicScheme();
+        authCache.put(target, basicAuth);
+
+        // Creating the CredentialsProvider and setting the credentials
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                new AuthScope(target),
+                new UsernamePasswordCredentials(user, password));
+
+        // Creating the HttpClientContext and assigning the AuthCache
+        HttpClientContext context = HttpClientContext.create();
+        context.setAuthCache(authCache);
+
+        URI uri = URI.create("http://" + host + ":" + port + "/" + databaseName);
+
+        HttpPut request = new HttpPut(uri);
 
         try {
-            HttpResponse response = httpClient.execute(request);
+
+            CloseableHttpClient httpClient = HttpClientBuilder.create().
+                    setDefaultCredentialsProvider(credentialsProvider).build();
+
+            HttpResponse response = httpClient.execute(request, context);
+
             int responseCode = response.getStatusLine().getStatusCode();
 
             if (responseCode == 201) {
                 System.out.println("Database " + databaseName + " created successfully.");
-            } else {
-                System.out.println("Error creating database " + databaseName + ". Response code: " + responseCode);
+                return;
             }
+            if (responseCode == 412) {
+                System.out.println("Database " + databaseName + " already exists.");
+                return;
+            }
+
+            System.out.println("Error creating database " + databaseName);
+            System.out.println("Response: " + responseCode + " " + response.getStatusLine().getReasonPhrase());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
