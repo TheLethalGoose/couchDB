1604e40900b1c5c0330e82b9510991ddfde9afb9
Fabian Teufel <fabianteufel@me.com>
2023-05-23 22:51:48 +0200

    Initializer defines not security rules for database

--- src/main/java/de/fh/dortmund/couchDB/couchInitializer.java
+++ src/main/java/de/fh/dortmund/couchDB/couchInitializer.java
@@ -8,6 +8,9 @@ import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
@@ -16,20 +19,25 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class couchInitializer {
 
-    public static void init(String host, int port, String databaseName, String user, String password) {
-
-        // Configuration of the target host
-        HttpHost target = new HttpHost(host, port, "http");
-
-        // On default couchDB only allows admin users to create databases
+    private static HttpClientContext httpClientContext(HttpHost target){
 
         // Creating the AuthCache and Basic authentication
         AuthCache authCache = new BasicAuthCache();
         BasicScheme basicAuth = new BasicScheme();
         authCache.put(target, basicAuth);
+
+        // Creating the HttpClientContext and assigning the AuthCache
+        HttpClientContext context = HttpClientContext.create();
+        context.setAuthCache(authCache);
+
+        return context;
+    }
+
+    private static CredentialsProvider getCredentialsProvider(String user, String password, HttpHost target){
 
         // Creating the CredentialsProvider and setting the credentials
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
@@ -37,38 +45,75 @@ public class couchInitializer {
                 new AuthScope(target),
                 new UsernamePasswordCredentials(user, password));
 
-        // Creating the HttpClientContext and assigning the AuthCache
-        HttpClientContext context = HttpClientContext.create();
-        context.setAuthCache(authCache);
+        return credentialsProvider;
+    }
 
-        URI uri = URI.create("http://" + host + ":" + port + "/" + databaseName);
+    private static HttpResponse createDatabase(URI databaseUri, HttpClientContext context, CredentialsProvider credentialsProvider) throws IOException {
 
-        HttpPut request = new HttpPut(uri);
+        HttpPut createRequest = new HttpPut(databaseUri);
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().
+                setDefaultCredentialsProvider(credentialsProvider).build();
+
+        return httpClient.execute(createRequest, context);
+    }
+
+    private static HttpResponse defineSecurityRules(URI databaseUri, HttpClientContext context, CredentialsProvider credentialsProvider) throws IOException {
+
+
+        String accessControlJson = "{\"admins\":{\"names\":[],\"roles\":[]},\"members\":{\"names\":[],\"roles\":[]}}";
+
+        HttpPut securityRequest = new HttpPut(databaseUri + "/_security");
+        StringEntity entity = new StringEntity(accessControlJson, ContentType.APPLICATION_JSON);
+        securityRequest.setEntity(entity);
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().
+                setDefaultCredentialsProvider(credentialsProvider).build();
+
+        return httpClient.execute(securityRequest, context);
+    }
+
+
+    public static void init(String host, int port, String databaseName, String user, String password) throws URISyntaxException {
+
+        // Configuration of the target host
+        HttpHost target = new HttpHost(host, port, "http");
+
+        // Creating the HttpClientContext
+        HttpClientContext context = httpClientContext(target);
+
+        // Creating the database URI
+        URI databaseUri = new URIBuilder().setScheme("http").setHost(host).setPort(port).setPath("/" + databaseName).build();
+
+        // Creating the CredentialsProvider
+        CredentialsProvider credentialsProvider = getCredentialsProvider(user, password, target);
 
         try {
 
-            CloseableHttpClient httpClient = HttpClientBuilder.create().
-                    setDefaultCredentialsProvider(credentialsProvider).build();
+            HttpResponse responseCreate = createDatabase(databaseUri, context, credentialsProvider);
+            int responseCodeCreate = responseCreate.getStatusLine().getStatusCode();
 
-            HttpResponse response = httpClient.execute(request, context);
-
-            int responseCode = response.getStatusLine().getStatusCode();
-
-            if (responseCode == 201) {
+            if (responseCodeCreate == 201) {
                 System.out.println("Database " + databaseName + " created successfully.");
+
+                HttpResponse responseSecurity = defineSecurityRules(databaseUri, context, credentialsProvider);
+
+                if(responseSecurity.getStatusLine().getStatusCode() == 200) {
+                    System.out.println("Security rules created successfully.");
+                }
+
                 return;
             }
-            if (responseCode == 412) {
+            if(responseCodeCreate == 412) {
                 System.out.println("Database " + databaseName + " already exists.");
                 return;
             }
 
             System.out.println("Error creating database " + databaseName);
-            System.out.println("Response: " + responseCode + " " + response.getStatusLine().getReasonPhrase());
+            System.out.println("Response Create: " + responseCodeCreate + " " + responseCreate.getStatusLine().getReasonPhrase());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
