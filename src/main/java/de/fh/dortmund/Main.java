fd2ce236957a0b67a4b298430d3ac207f6c526f6
Fabian Teufel <fabianteufel@me.com>
2023-05-23 22:53:16 +0200

    Updated main

--- src/main/java/de/fh/dortmund/Main.java
+++ src/main/java/de/fh/dortmund/Main.java
@@ -1,11 +1,23 @@
 package de.fh.dortmund;
 
 import de.fh.dortmund.couchDB.couchInitializer;
+import de.fh.dortmund.fakedata.user.UserGenerator;
+import de.fh.dortmund.models.User;
+import lombok.SneakyThrows;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    @SneakyThrows
     public static void main(String[] args) {
 
         couchInitializer.init("localhost", 5984, "stackoverflow", "admin", "admin");
+
+        List<User> users = new ArrayList<>();
+
+        UserGenerator.generateUsers(users, 1000, true);
 
     }
 }
