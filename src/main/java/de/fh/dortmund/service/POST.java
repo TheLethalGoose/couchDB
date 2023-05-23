ccff2f32e1b9efd52ee1762201cdfe8b500c514f
Fabian Teufel <fabianteufel@me.com>
2023-05-23 22:52:53 +0200

    Option to bulk post lists

--- src/main/java/de/fh/dortmund/service/POST.java
+++ src/main/java/de/fh/dortmund/service/POST.java
@@ -1,16 +1,17 @@
 package de.fh.dortmund.service;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
+import java.util.List;
 
 public class POST {
 
@@ -19,22 +20,38 @@ public class POST {
     String databaseName;
 
     public POST(String databaseName) {
-        this.databaseName = databaseName + "/";
+        this.databaseName = databaseName;
     }
 
-    public int POST(Object object) {
+    public HttpResponse post(Object object) throws IOException {
 
-        HttpClient httpClient = HttpClientBuilder.create().build();
         HttpPost request = new HttpPost("http://localhost:5984/" + databaseName);
-        request.setEntity(new StringEntity(gson.toJson(object), ContentType.APPLICATION_JSON));
-        HttpResponse response = null;
-        try {
-            response = httpClient.execute(request);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        return response.getStatusLine().getStatusCode();
+        request.setEntity(new StringEntity(gson.toJson(object), ContentType.APPLICATION_JSON));
+
+        return executeRequest(request);
 
     }
+    public HttpResponse post(List<?> objects) throws IOException {
+
+
+        HttpPost request = new HttpPost("http://localhost:5984/" + databaseName + "/_bulk_docs");
+
+        JsonObject jsonObject = new JsonObject();
+        JsonArray jsonArray = new Gson().toJsonTree(objects).getAsJsonArray();
+        jsonObject.add("docs", jsonArray);
+
+        request.setEntity(new StringEntity(jsonObject.toString(), ContentType.APPLICATION_JSON));
+
+        return executeRequest(request);
+
+    }
+
+    private HttpResponse executeRequest(HttpPost request) throws IOException {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+
+        return httpClient.execute(request);
+
+    }
+
 }
