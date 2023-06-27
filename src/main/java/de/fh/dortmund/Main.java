b94534804be1a5e1cba5c05a35b1a1d7b9ec4559
Fabian Teufel <fabianteufel@me.com>
2023-06-28 01:01:07 +0200

    Updated Persistor and Main

--- src/main/java/de/fh/dortmund/Main.java
+++ src/main/java/de/fh/dortmund/Main.java
@@ -8,30 +8,35 @@ import de.fh.dortmund.fakedata.generator.post.QuestionGenerator;
 import de.fh.dortmund.fakedata.generator.tag.TagGenerator;
 import de.fh.dortmund.fakedata.generator.user.UserGenerator;
 import de.fh.dortmund.fakedata.generator.vote.VoteGenerator;
-import de.fh.dortmund.helper.Timer;
 import de.fh.dortmund.models.*;
-import de.fh.dortmund.service.POST;
 import lombok.SneakyThrows;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
+
+    private static final String COUCHDB_HOST = "localhost";
+    private static final int COUCHDB_PORT = 5984;
+
+    private static final String COUCHDB_DATABASE = "stackoverflow";
+
+    public static String COUCHDB_URL = "http://" + COUCHDB_HOST + ":" + COUCHDB_PORT + "/" + COUCHDB_DATABASE;
+
+
     private static final int AMOUNT_QUESTIONS = 1500;
     private static final int AMOUNT_ANSWERS = 2000;
     private static final int AMOUNT_TAGS = 20;
     private static final int AMOUNT_COMMENTS = 3000;
-    private static final int AMOUNT_USERS = 5;
+    private static final int AMOUNT_USERS = 1000;
     private static final int TAG_AMOUNT_PER_QUESTION = 5;
     public static final String DATABASE_NAME = "stackoverflow";
-    private static POST POST = new POST(DATABASE_NAME);
-    static Timer timer = new Timer();
-
-    private static final CouchPersistor persistor = new CouchPersistor();
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        CouchInitializer.init("localhost", 5984, DATABASE_NAME, "admin", "admin", true);
+
+        CouchInitializer.init(COUCHDB_HOST, COUCHDB_PORT, DATABASE_NAME, "admin", "admin", true);
 
         // Generate test data
         List<User> users = UserGenerator.generateUsers(AMOUNT_USERS);
@@ -42,11 +47,37 @@ public class Main {
         List<Vote> votes = VoteGenerator.generateVotes(comments, questions, answers, users);
 
         // Insert data into database
-        persistor.persist(users, "users", true, AMOUNT_USERS);
-        persistor.persist(tags, "tags", true, AMOUNT_TAGS);
-        persistor.persist(questions, "questions", true, AMOUNT_QUESTIONS);
-        persistor.persist(answers, "answers", true, AMOUNT_ANSWERS);
-        persistor.persist(comments, "comments", true, AMOUNT_COMMENTS);
-        persistor.persist(votes, "votes", true, votes.size());
+        CouchPersistor.persist(users, "users", true, AMOUNT_USERS);
+        CouchPersistor.persist(tags, "tags", true, AMOUNT_TAGS);
+        CouchPersistor.persist(questions, "questions", true, AMOUNT_QUESTIONS);
+        CouchPersistor.persist(answers, "answers", true, AMOUNT_ANSWERS);
+        CouchPersistor.persist(comments, "comments", true, AMOUNT_COMMENTS);
+        CouchPersistor.persist(votes, "votes", true, Objects.requireNonNull(votes).size());
+
+
+        //System.out.println(getDocsFromView("_design/tags","allTags",true, true)); //REDUCE FUNCTION
+        //System.out.println(getDocsFromView("_design/tags","popularTags",true, true)); //REDUCE FUNCTION
+
+        //System.out.println(getDocsFromView("_design/users","allUsers"));
+        //System.out.println(getDocsFromView("_design/users","byEmail","alexis.strosin@yahoo.com"));
+
+        //System.out.println(getDocsFromView("_design/votes","allVotes"));
+        //System.out.println(getDocsFromView("_design/votes","totalVotesToQuestion", "0dbb4013-a135-4dfe-85c1-30009d071e2e",true)); //REDUCE FUNCTION
+        //System.out.println(getDocsFromView("_design/votes","votesToPost", "0dbb4013-a135-4dfe-85c1-30009d071e2e"));
+
+        //System.out.println(getDocsFromView("_design/posts","allQuestions"));
+        //System.out.println(getDocsFromView("_design/posts","allAnswers"));
+        //System.out.println(getDocsFromView("_design/posts","allComments"));
+        //System.out.println(getDocsFromView("_design/posts","allPosts"));
+        //System.out.println(getDocsFromView("_design/posts","answersToQuestion", "0dbb4013-a135-4dfe-85c1-30009d071e2e"));
+        //System.out.println(getDocsFromView("_design/posts","byUserId", "008c203b-c737-424d-82cf-9b56992ea743"));
+        //System.out.println(getDocsFromView("_design/posts","byTagName", "application"));
+        //System.out.println(getDocsFromView("_design/posts","latestQuestions"));
+
+        //addPostToFavorites("00945701-66eb-4d1f-b84f-b347f5038cef", "0dbb4013-a135-4dfe-85c1-30009d071e2e");
+        //System.out.println(getDocsFromView("_design/users","userFavorites", "00945701-66eb-4d1f-b84f-b347f5038cef"));
+        //updatePost("0dbb4013-a135-4dfe-85c1-30009d071e2e", "FML");
+        //markAnswerAsAccepted("002e7a4b-ee51-4d2b-9577-faee515eabeb");
+
     }
 }
--- src/main/java/de/fh/dortmund/couchDB/CouchPersistor.java
+++ src/main/java/de/fh/dortmund/couchDB/CouchPersistor.java
@@ -1,23 +1,22 @@
 package de.fh.dortmund.couchDB;
 
 import de.fh.dortmund.helper.Timer;
-import de.fh.dortmund.service.POST;
 
 import java.io.IOException;
 import java.util.List;
 
-import static de.fh.dortmund.Main.DATABASE_NAME;
+import static de.fh.dortmund.service.POST.post;
 
 
 public class CouchPersistor {
-    private static POST POST = new POST(DATABASE_NAME);
+
     private static Timer timer = new Timer();
 
     public static void persist(List<?> objects, String createdObjectName, boolean debug, int amount){
         timer.start();
 
         try {
-            POST.post(objects);
+            post(objects);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -28,6 +27,5 @@ public class CouchPersistor {
             System.out.println("Created " + amount + " " +  createdObjectName + " in " + timeToCreate + "ms.");
         }
     }
-
 
 }
